06c635b7f19afb526f82634600da151c0e2f14b7
Owl Software <owlsoftwaremanager@gmail.com>
2023-10-01 12:56:58 +0300

    Scraping successfully
    Issue with not loading inside scripts on page

--- NodeJs-Express/routes/web-scraper.js
+++ NodeJs-Express/routes/web-scraper.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const cheerio = require('cheerio');
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 
 /* GET webScraping url. */
@@ -10,7 +10,8 @@ router.post('/', async (req, res) => {
     res.set('Access-Control-Allow-Origin', 'http://localhost:4200');
     let data = req.body;
     if(data['targetUrl'] != null || data['targetUrl'] != undefined){
-        res.send(await scrapeWeb(data['targetUrl']))
+        //res.send(await scrapeWeb(data['targetUrl']))
+        res.send(await scrapeWebAxios(data['targetUrl']))
     }else{
         res.send('No founded url key');
     }
@@ -27,8 +28,23 @@ async function scrapeWeb(targetUrl){
             throw new Error('Server error');
             }
         })
-        .then(json => console.log(json))
-        .catch(err => console.error(err));
-    }
+    .then(json => console.log(json))
+    .catch(err => console.error(err));
+}
+
+async function scrapeWebAxios(targetUrl){
+    const axiosResponse = await axios.request({
+        method: "GET",
+        url: targetUrl,
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+        }
+    })
+    //return axiosResponse.data;
+    const $ = cheerio.load(axiosResponse.data)
+    return $.text();
+    var value = $('#__next').text();
+    return value;
+}
 
 module.exports = router;
